16967f976860e0d3ecb7e903198b0d6c56fdf57f
酱紫 <602426011@qq.com>
2017-04-09 16:01:30 +0800

    调整保存代码

--- src/option.js
+++ src/option.js
@@ -1,33 +1,35 @@
 $(document).ready(function() {
-	var favorite = localStorage["path"];
-	if (favorite) {
-		$('#path').val(favorite);
-	}
-	favorite = localStorage["size"];
-	if (favorite) {
-		$('#size').val(favorite);
-	}
-	$("#size").blur(function() {
-		save_options('size');
-	});
-	$("#path").blur(function() {
-		save_options('path');
-	});
-	function save_options(name) {
-		this.tmp = $.trim($('#' + name).val());
-		if (!this.tmp) {
-			show(name, "不可为空");
-			$('#' + name).focus();
-			return false;
-		} else {
-			localStorage[name] = this.tmp;
-			show(name, "已保存");
-		}
-	}
-	function show(name, msg) {
-		$('#' + name).next('span').html(msg);
-		setTimeout(function() {
-			$('#' + name).next('span').html('');
-		}, 3000);
-	}
+    var favorite = localStorage["path"];
+    if (favorite) {
+        $('#path').val(favorite);
+    }
+    favorite = localStorage["size"];
+    if (favorite) {
+        $('#size').val(favorite);
+    }
+
+    $("#save").click(function() {
+        save_options('size');
+        save_options('path');
+
+        window.console.log(localStorage["path"], localStorage["size"]);
+    });
+    function save_options(name) {
+        this.tmp = $.trim($('#' + name).val());
+        if (!this.tmp) {
+            show(name, "不可为空");
+            $('#' + name).focus();
+            return false;
+        } else {
+            localStorage[name] = this.tmp;
+            show(name, "已保存");
+        }
+    }
+    function show(name, msg) {
+        $('#' + name).next('span').html(msg);
+        setTimeout(function() {
+            $('#' + name).next('span').html('');
+        },
+        3000);
+    }
 });
